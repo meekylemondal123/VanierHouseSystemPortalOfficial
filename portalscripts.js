bcebf1faf0a68b5169c7f471d849e10fba9b72ac
Meekyle Mondal <ammeekyle.mondal@student.tdsb.on.ca>
2020-07-07 10:56:37 -0400

    Add files via upload

---
+++ portalscripts.js
@@ -0,0 +1,41 @@
+// This is where the name searching javascript will go, refer to the github to find the code
+const filePath = "Names.txt";
+let result;
+
+function loadFile(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+
+    if (xmlhttp.status == 200){
+        result = xmlhttp.responseText;
+        result = result.split("\n");
+    }
+}
+
+function findHouseTeam(){
+    let userInput = document.getElementById("user-input-text").value;
+
+    for (let i = 0; i < result.length; i++){
+        if (result[i].toLowerCase().includes(userInput.toLowerCase())){
+            let userArray = result[i].split(" ");
+            document.getElementById("which-house-image").src = userArray[userArray.length - 1] + " Description.png";
+        }
+    }
+}
+
+function flipImage(houseTeamName){
+    var img = document.getElementById(houseTeamName);
+    var fileName = "Vanier_House_" + houseTeamName + "PNG.png";
+
+
+    if (img.getAttribute("src") === fileName){
+        img.src = houseTeamName + " Quote.png";
+    } else {
+        img.src = fileName;
+    }
+}
+
+function clearName(){
+    document.getElementById("user-input-text").value = "";
+}
